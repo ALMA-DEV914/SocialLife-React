cc878a8d733dfd2fcf9e7cbcbe19babe0e4181bf
ALMA_DEV914 <aungonalna58@gamil.com>
2022-03-10 13:57:32 -0800

    add post and share components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import Home from './pages/home/Home';
+import TopBar from './components/topbar/Topbar';
+import Feed from './components/feed/Feed';
 import Sidebar from './components/sidebar/Sidebar';
 import Rightbar from './components/rightbar/Rightbar';
 
 function App() {
   return (
     <>
-   <Home/>
+   <TopBar/>
    <div className='homeConatiner'>
    <Sidebar/>
+   <Feed/>
    <Rightbar/>
    </div>
    </>
