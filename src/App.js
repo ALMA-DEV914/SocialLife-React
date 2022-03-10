044e41cb5507cf6015c8c8b2154e75e433f5d787
ALMA_DEV914 <aungonalna58@gamil.com>
2022-03-10 13:36:02 -0800

    add sidebar components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Home from './pages/home/Home';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
+    <>
    <Home/>
+   <div className='homeConatiner'>
+   <Sidebar/>
+   </div>
+   </>
   );
 }
 
