466837ff4e1b663a2f07603bb2bdffc49fb0b398
ALMA_DEV914 <aungonalna58@gamil.com>
2022-03-10 15:36:58 -0800

    update login and register pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+
 function App() {
   return (
      <Profile/>
